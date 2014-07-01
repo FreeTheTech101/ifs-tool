798d6960068c0a30a8fcd3415b21c2fce63b32b6
Maurice Heumann <MauriceHeumann@gmail.com>
2014-07-01 21:45:59 +0200

    lol

--- IFS Tool/IFS Tool.cpp
+++ IFS Tool/IFS Tool.cpp
@@ -101,7 +101,7 @@ void handleFile()
 	SFileOpenArchive = (SFileOpenArchive_w_t)((DWORD)ifs2_lib + 0x16230);
 	SFileExtractFile = (SFileExtractFile_w_t)((DWORD)ifs2_lib + 0x24BA0);
 	SFileCloseFile   = (SFileCloseFile_t)((DWORD)ifs2_lib + 0x1FFB0);
-	SFileReadFile_w    = (SFileReadFile_w_t)((DWORD)ifs2_lib + 0x24880);
+	SFileReadFile_w  = (SFileReadFile_w_t)((DWORD)ifs2_lib + 0x24880);
 	SFileReadFile    = (SFileReadFile_t)((DWORD)ifs2_lib + 0x215D0);
 	NIFSOpenFileEx   = (NIFSOpenFileEx_t)((DWORD)ifs2_lib + 0x1EDE0);
 
